23757baef92e4790f4d1f464356f247a78378f29
Chris Patuzzo <chris@patuzzo.co.uk>
2025-02-02 13:28:35 +0000

    Avoid using a sparse array for squares_by_residue_class
    It doesn’t really affect performance, but I think it’s more intention
    revealing to use a 3-element array for squares_by_residue* rather than
    a 72-element array since only 3 elements will actually be populated
    corresponding to residues 1, 25 and 49 modulo 72.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
-// These are the residues modulo 72 that are congruent to 1 modulo 24.
-const RESIDUES: [u8; 3] = [1, 25, 49];
-
 fn main() {
-    let mut squares_by_residue = [const { vec![] }; 72];
+    let mut squares_by_residue_class = [vec![], vec![], vec![]];
 
     for number in 1_u64.. {
-        let square1 = number * number;
-
         // Skip perfect squares that are not congruent to 1 modulo 24.
+        let square1 = number * number;
         if square1 % 24 != 1 { continue; }
-        let residue1 = (square1 % 72) as u8;
 
-        for &residue2 in &RESIDUES {
-            // Skip magic sums that are not congruent to 3 modulo 72.
-            let squares2 = &squares_by_residue[residue2 as usize];
-            let residue3 = (147 - residue1 - residue2) % 72;
+        // Map the residues modulo 72 (1, 25, 49) to indexes (0, 1, 2).
+        let residue_class1 = (square1 % 72 / 24) as usize;
+
+        for (residue_class2, squares2) in squares_by_residue_class.iter().enumerate() {
+            let residue_class3 = (6 - residue_class1 - residue_class2) % 3;
 
             // Skip enumerating symmetries in B + C when the arrays are equal.
-            if residue2 == residue3 {
+            if residue_class2 == residue_class3 {
                 for (i, square2) in squares2.iter().enumerate() {
                     let partial_sum = square1 + square2;
 
@@ -28,7 +24,7 @@ fn main() {
                 }
             // Otherwise, enumerate all pairwise combinations of the arrays.
             } else {
-                let squares3 = &squares_by_residue[residue3 as usize];
+                let squares3 = &squares_by_residue_class[residue_class3];
 
                 for square2 in squares2 {
                     let partial_sum = square1 + square2;
@@ -42,6 +38,6 @@ fn main() {
         }
 
         // Skip duplicate perfect squares by pushing after the for loop.
-        squares_by_residue[residue1 as usize].push(square1);
+        squares_by_residue_class[residue_class1].push(square1);
     }
 }
