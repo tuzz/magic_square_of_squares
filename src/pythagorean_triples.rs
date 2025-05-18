796ee48a0144766b90fe7fe38f6d89d3191454ed
Chris Patuzzo <chris@patuzzo.co.uk>
2025-05-18 19:10:13 +0100

    Calculate the product of primitive Pythagorean triples

--- src/pythagorean_triples.rs
+++ src/pythagorean_triples.rs
@@ -38,6 +38,57 @@ impl PythagoreanTriples {
         self.a_values.len()
     }
 
+    pub fn resize(&mut self, new_len: usize, value: u64) {
+        self.a_values.resize(new_len, value);
+        self.b_values.resize(new_len, value);
+        self.c_values.resize(new_len, value);
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            a_values: Vec::with_capacity(capacity),
+            b_values: Vec::with_capacity(capacity),
+            c_values: Vec::with_capacity(capacity),
+        }
+    }
+
+    pub fn push(&mut self, (a, b, c): (u64, u64, u64)) {
+        self.a_values.push(a);
+        self.b_values.push(b);
+        self.c_values.push(c);
+    }
+
+    // Apply the Brahmagupta–Fibonacci identity to combine two Pythagorean triples
+    // into two new primitive Pythagorean triples for the product of hypotenuses.
+    pub fn product(&self, (x, y, z): (u64, u64, u64), output: &mut Self) {
+        let num_triples = self.len();
+        let existing_len = output.len();
+        output.resize(existing_len + num_triples * 2, 0);
+
+        for i in 0..num_triples {
+            let a = self.a_values[i];
+            let b = self.b_values[i];
+            let c = self.c_values[i];
+
+            let ax = a * x;
+            let ay = a * y;
+            let bx = b * x;
+            let by = b * y;
+            let cz = c * z;
+
+            let first_slot = existing_len + i * 2;
+            let second_slot = first_slot + 1;
+
+            output.a_values[first_slot] = ax.abs_diff(by);
+            output.b_values[first_slot] = ay + bx;
+            output.c_values[first_slot] = cz;
+
+            output.a_values[second_slot] = ax + by;
+            output.b_values[second_slot] = ay.abs_diff(bx);
+            output.c_values[second_slot] = cz;
+        }
+    }
+
     // Use Cornacchia's algorithm to solve a^2 + b^2 = p then apply Euclid's
     // parameterization to find the primitive Pythagorean triple for the prime.
     fn compute(pythagorean_prime: u64) -> (u64, u64) {
@@ -113,6 +164,31 @@ mod test {
             let a = triples.a_values[i];
             let b = triples.b_values[i];
             let c = triples.c_values[i];
+            assert_eq!(a * a + b * b, c * c);
+        }
+    }
+
+    #[test]
+    fn it_can_calculate_the_product_of_primitive_triples() {
+        let mut triples = PythagoreanTriples::new(100);
+        let mut output = PythagoreanTriples::with_capacity(203);
+
+        // Existing triples in output should be preserved.
+        output.push((3, 4, 5));
+        output.push((5, 12, 13));
+        output.push((15, 8, 17));
+
+        triples.product((3, 4, 5), &mut output);
+        assert_eq!(output.len(), 203);
+
+        assert_eq!(&output.a_values[0..8], &[3, 5, 15, 7, 25, 33, 63, 13]);
+        assert_eq!(&output.b_values[0..8], &[4, 12, 8, 24, 0, 56, 16, 84]);
+        assert_eq!(&output.c_values[0..8], &[5, 13, 17, 25, 25, 65, 65, 85]);
+
+        for i in 0..output.len() {
+            let a = output.a_values[i];
+            let b = output.b_values[i];
+            let c = output.c_values[i];
             assert_eq!(a * a + b * b, c * c);
         }
     }
