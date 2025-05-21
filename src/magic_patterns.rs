bb2724759e9b3b408504ed16d0125edf72b0e7ae
Chris Patuzzo <chris@patuzzo.co.uk>
2025-05-21 23:39:09 +0100

    Refactor magic_patterns.rs

--- src/magic_patterns.rs
+++ src/magic_patterns.rs
@@ -2,24 +2,8 @@ use std::cell::RefCell;
 
 // The patterns are from figure 5 of http://www.multimagie.com/Search.pdf#page=2
 
-pub struct Cell {
-    pub value: u128,
-    pub magic_sum_minus_value: u128,
-}
-
-impl Cell {
-    pub fn new(value: u128, magic_sum: u128) -> Self {
-        Self { value, magic_sum_minus_value: magic_sum - value }
-    }
-
-    pub fn squared(n: u64, magic_sum: u128) -> Self {
-        let n = n as u128;
-        Self::new(n * n, magic_sum)
-    }
-}
-
 thread_local! {
-    static CELLS: RefCell<(Vec<Cell>, Vec<Cell>)> = const { RefCell::new((vec![], vec![])) };
+    static SQUARES: RefCell<(Vec<u128>, Vec<u128>)> = const { RefCell::new((vec![], vec![])) };
 }
 
 pub fn check_magic_patterns(a_values: &[u64], b_values: &[u64], c: u64) {
@@ -29,78 +13,113 @@ pub fn check_magic_patterns(a_values: &[u64], b_values: &[u64], c: u64) {
     let center_square = center * center;
     let center_sum = center_square + center_square;
     let magic_sum = center_sum + center_square;
-    let center_cell = Cell::new(center_square, magic_sum);
 
-    CELLS.with_borrow_mut(|(a_cells, b_cells)| {
-        a_cells.clear();
-        a_cells.extend(a_values.iter().map(|&a| Cell::squared(a, magic_sum)));
+    SQUARES.with_borrow_mut(|(a_squares, b_squares)| {
+        a_squares.clear();
+        a_squares.extend(a_values.iter().map(|&a| { let a = a as u128; a * a }));
 
-        b_cells.clear();
-        b_cells.extend(b_values.iter().map(|&b| Cell::squared(b, magic_sum)));
+        b_squares.clear();
+        b_squares.extend(b_values.iter().map(|&b| { let b = b as u128; b * b }));
 
-        for (i, (a_cell1, b_cell1)) in a_cells.iter().zip(b_cells.iter()).enumerate() {
-            let other_a_cells = &a_cells[i + 1..];
-            let other_b_cells = &b_cells[i + 1..];
+        for (i, (&a_square1, &b_square1)) in a_squares.iter().zip(b_squares.iter()).enumerate() {
+            let other_a_squares = &a_squares[i + 1..];
+            let other_b_squares = &b_squares[i + 1..];
 
-            let upto_index = other_a_cells.partition_point(|a_cell| a_cell.value < a_cell1.magic_sum_minus_value);
+            let a_remainder = magic_sum - a_square1;
+            let b_remainder = magic_sum - b_square1;
+            let b_minimum = a_square1 - center_square;
 
-            // a_cell1 and a_cell2 may be on the same line:
-            for (a_cell2, b_cell2) in other_a_cells[..upto_index].iter().zip(&other_b_cells[..upto_index]) {
-                check_patterns_1_and_2(&center_cell, a_cell1, b_cell1, a_cell2, b_cell2);
-                check_patterns_3_4_and_6(&center_cell, a_cell1, b_cell1, b_cell2, a_cell2);
-                check_patterns_3_4_and_6(&center_cell, a_cell2, b_cell2, b_cell1, a_cell1);
+            let a_upto = other_a_squares.partition_point(|&s| s < a_remainder);
+            let b_upto = other_b_squares.partition_point(|&s| s >= b_minimum);
+
+            for (&a_square2, &b_square2) in other_a_squares[..a_upto].iter().zip(&other_b_squares[..a_upto]) {
+                let aa_candidate = a_remainder - a_square2;
+                if is_square(aa_candidate) {
+                    check_pattern_2(aa_candidate, a_square1, b_square1, a_square2, b_square2, center_square);
+                    check_pattern_3_and_4(aa_candidate, a_square1, a_square2, b_square1, b_square2, center_square, magic_sum, center_sum);
+                }
             }
 
-            // a_cell1 and b_cell2 may be on the same line:
-            for (a_cell2, b_cell2) in other_a_cells.iter().zip(other_b_cells) {
-                check_patterns_3_4_and_6(&center_cell, a_cell1, b_cell1, a_cell2, b_cell2);
-                check_patterns_3_4_and_6(&center_cell, b_cell2, a_cell2, b_cell1, a_cell1);
+            for (&a_square2, &b_square2) in other_a_squares.iter().zip(other_b_squares) {
+                let ab_candidate = a_remainder - b_square2;
+                if is_square(ab_candidate) {
+                    check_pattern_3_and_4(ab_candidate, a_square1, b_square2, b_square1, a_square2, center_square, magic_sum, center_sum);
+                }
+
+                let ba_candidate = b_remainder - a_square2;
+                if is_square(ba_candidate) {
+                    check_pattern_3_and_4(ba_candidate, b_square1, a_square2, a_square1, b_square2, center_square, magic_sum, center_sum);
+                }
             }
+
+            for (&a_square2, &b_square2) in other_a_squares[..b_upto].iter().zip(&other_b_squares[..b_upto]) {
+                let bb_candidate = b_remainder - b_square2;
+                if is_square(bb_candidate) {
+                    check_pattern_2(bb_candidate, a_square1, b_square1, a_square2, b_square2, center_square);
+                    check_pattern_3_and_4(bb_candidate, b_square1, b_square2, a_square1, a_square2, center_square, magic_sum, center_sum);
+                }
+            }
+
+            // TODO: pattern 6
         }
     });
 }
 
-fn check_patterns_1_and_2(center: &Cell, top_left: &Cell, bottom_right: &Cell, top_right: &Cell, bottom_left: &Cell) {
-    let top_middle = top_left.magic_sum_minus_value - top_right.value;
-    let middle_left = top_left.magic_sum_minus_value - bottom_left.value;
-    let Some(bottom_middle) = bottom_left.magic_sum_minus_value.checked_sub(bottom_right.value) else { return };
+fn check_pattern_2(top_middle: u128, a_square1: u128, b_square1: u128, a_square2: u128, b_square2: u128, center_square: u128) {
+    let middle_left = a_square1 - b_square1;
+    if is_square(middle_left) {
+        print(a_square1, top_middle, a_square2, middle_left, center_square, 0, b_square1, 0, b_square2);
+    }
 
-    let mut num_squares = 5;
-    if is_square(top_middle) { num_squares += 1; }
-    if is_square(middle_left) { num_squares += 1; }
-    if is_square(bottom_middle) { num_squares += 1; }
-    if num_squares < 6 { return; }
+    let middle_left = a_square1 - b_square2;
+    if is_square(middle_left) {
+        print(a_square1, top_middle, a_square2, middle_left, center_square, 0, b_square1, 0, b_square2);
+    }
 
-    let middle_right = center.magic_sum_minus_value - middle_left;
-    if is_square(middle_right) { num_squares += 1; }
-    if num_squares < 7 { return; }
+    let middle_left = a_square2 - b_square1;
+    if is_square(middle_left) {
+        print(a_square1, top_middle, a_square2, middle_left, center_square, 0, b_square1, 0, b_square2);
+    }
 
-    println!("{} | {} | {}", top_left.value, top_middle, top_right.value);
-    println!("{} | {} | {}", middle_left, center.value, middle_right);
-    println!("{} | {} | {}\n", bottom_left.value, bottom_middle, bottom_right.value);
+    let middle_left = a_square2 - b_square2;
+    if is_square(middle_left) {
+        print(a_square1, top_middle, a_square2, middle_left, center_square, 0, b_square1, 0, b_square2);
+    }
 }
 
-fn check_patterns_3_4_and_6(center: &Cell, top_right: &Cell, bottom_left: &Cell, middle_left: &Cell, middle_right: &Cell) {
-    let top_left = bottom_left.magic_sum_minus_value - middle_left.value;
-    let bottom_right = top_right.magic_sum_minus_value - middle_right.value;
-    let Some(top_middle) = top_right.magic_sum_minus_value.checked_sub(top_left) else { return };
+fn check_pattern_3_and_4(top_left: u128, left_square1: u128, left_square2: u128, right_square1: u128, right_square2: u128, center_square: u128, magic_sum: u128, center_sum: u128) {
+    let top_middle = magic_sum - top_left - right_square1;
+    if is_square(top_middle) {
+        print(top_left, top_middle, right_square1, left_square2, center_square, right_square2, left_square1, 0, 0);
+    } else {
+        let bottom_middle = center_sum - top_middle;
+        if is_square(bottom_middle) {
+            print(top_left, 0, right_square1, left_square2, center_square, right_square2, left_square1, bottom_middle, 0);
+        }
+    }
 
-    let mut num_squares = 5;
-    if is_square(top_left) { num_squares += 1; }
-    if is_square(bottom_right) { num_squares += 1; }
-    if is_square(top_middle) { num_squares += 1; }
-    if num_squares < 6 { return; }
-
-    let Some(bottom_middle) = bottom_left.magic_sum_minus_value.checked_sub(bottom_right) else { return };
-    if is_square(bottom_middle) { num_squares += 1; }
-    if num_squares < 7 { return; }
-
-    println!("{} | {} | {}", top_left, top_middle, top_right.value);
-    println!("{} | {} | {}", middle_left.value, center.value, middle_right.value);
-    println!("{} | {} | {}\n", bottom_left.value, bottom_middle, bottom_right);
+    let top_middle = magic_sum - top_left - right_square2;
+    if is_square(top_middle) {
+        print(top_left, top_middle, right_square2, left_square1, center_square, right_square1, left_square2, 0, 0);
+    } else {
+        let bottom_middle = center_sum - top_middle;
+        if is_square(bottom_middle) {
+            print(top_left, 0, right_square1, left_square2, center_square, right_square2, left_square1, bottom_middle, 0);
+        }
+    }
 }
 
 fn is_square(n: u128) -> bool {
     let root = n.isqrt();
     root * root == n
+}
+
+fn print(top_left: u128, top_middle: u128, top_right: u128, middle_left: u128, middle_middle: u128, middle_right: u128, bottom_left: u128, bottom_middle: u128, bottom_right: u128) {
+    println!("----------------------------------------------------------------------------------------------------");
+    println!("| {top_left:^30} | {top_middle:^30} | {top_right:^30} |");
+    println!("|--------------------------------------------------------------------------------------------------|");
+    println!("| {middle_left:^30} | {middle_middle:^30} | {middle_right:^30} |");
+    println!("|--------------------------------------------------------------------------------------------------|");
+    println!("| {bottom_left:^30} | {bottom_middle:^30} | {bottom_right:^30} |");
+    println!("----------------------------------------------------------------------------------------------------\n\n");
 }
